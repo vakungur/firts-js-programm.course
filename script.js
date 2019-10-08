4541892262f86a659ba01b341e3a286eb48abce9
vakungur <vakungur@gmail.com>
2019-10-09 00:15:23 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+/* var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14 */
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bnp'];
+
+// console.log(arr[2]);
+
+// alert("Hello, world!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = prompt("Есть ли Вам 18?", "Да");
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose =  true;
+
+console.log(isChecked &&(/* || */) isClose);
