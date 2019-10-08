811cf8091b41ddb344b7d3ccfff838ce7f608242
vakungur <vakungur@gmail.com>
2019-10-09 00:18:59 +0300

    Это наш второй комит

--- script.js
+++ script.js
@@ -27,20 +27,20 @@ const pi = 3.14 */
 
 // console.log("arr" + " - object");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
 
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-console.log(5%2);
+// console.log(5%2);
 
-console.log("2" == 2);
-console.log("2" === 2);
+// console.log("2" == 2);
+// console.log("2" === 2);
 
-let isChecked = true,
-    isClose =  true;
+// let isChecked = true,
+//     isClose =  true;
 
-console.log(isChecked &&(/* || */) isClose);
+// console.log(isChecked &&(/* || */) isClose);
