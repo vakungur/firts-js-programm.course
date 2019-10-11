35e076a5111cb751e0cbc7f0d2375c3a0c395483
vakungur <vakungur@gmail.com>
2019-10-12 02:26:37 +0300

    Продлжаю работу с DOM(Изменение страницы с помощью JS)

--- training/DOM/script.js
+++ training/DOM/script.js
@@ -3,12 +3,40 @@ let box = document.getElementById('box'),
     circle = document.getElementsByClassName('circle'),
     heart = document.querySelectorAll('.heart'),
     oneHeart = document.querySelector('.heart');
-
-
-
-console.log(box);    
+    wrapper = document.querySelector('.wrapper');
+/* console.log(box);    
 console.log(btn);
 console.log(btn[0]);
 console.log(circle[2]);
 console.log(heart[2]);
-console.log(oneHeart);
+console.log(oneHeart); */
+box.style.backgroundColor = 'blue';
+btn[0].style.borderRadius = '100%';
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
+
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = 'blue';
+// }
+
+// heart.forEach(function(item, i, hearts) {
+//     item.style.backgroundColor = 'blue';
+// });
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+// document.body.appendChild(div);
+// wrapper.appendChild(div);
+
+// div.innerHTML = '<h1>Hello world!</h1>';
+    div.textContent = 'Hello world!';
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+wrapper.removeChild(heart[1]);
+
+document.body.replaceChild(btn[1], circle[1]);
+
