2349a3c829637c11d86bb0d9e6188add863d934b
vakungur <vakungur@gmail.com>
2019-11-10 01:19:14 +0300

    Изучаю промисы

--- project2/yoga/js/script.js
+++ project2/yoga/js/script.js
@@ -113,19 +113,14 @@ window.addEventListener('DOMContentLoaded', function(){
 
         let request = new XMLHttpRequest();
         request.open('POST', 'server.php');
-        // request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded');
         request.setRequestHeader('Content-type', 'application/json; charset-8');
-
         let formData = new FormData(form);
-
         let obj = {};
         formData.forEach(function(value, key){
             obj[key] = value;
         });
         let json = JSON.stringify(obj);
-
         request.send(json);
-
         request.addEventListener('readystatechange', function() {
             if (request.readyState < 4) {
                 statusMessage.innerHTML = message.loading;
@@ -135,7 +130,6 @@ window.addEventListener('DOMContentLoaded', function(){
                 statusMessage.innerHTML = message.failure;
             }
         });
-
             for (let i = 0; i < input.length; i++) {
                 input[i].value = '';
             }
---
+++ training/promises/main.js
@@ -0,0 +1,76 @@
+// с промисом
+let drink = 1;
+
+function shoot(arrow) {
+    console.log("Вы сделали выстрел...");
+    let promise = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            Math.random() > .5 ? resolve({}) : reject("Вы промахнулись");
+        }, 3000)
+    });
+    
+    return promise;
+};
+
+function win() {
+    console.log('Вы победили!');
+    (drink == 1) ? buyBeer() : giveMoney();
+}
+
+function buyBeer() {
+    console.log('Вам купили пива!');
+}
+
+function giveMoney() {
+    console.log('Вам заплатили!');
+}
+
+function loose() {
+    console.log('Вы програли!');
+}
+
+shoot({})
+        .then(mark => console.log('Вы попали в цель!'))
+        .then(win)
+        .catch(loose)
+
+
+
+// // без промиса
+// let drink = 1;
+
+// function shoot(arrow, headshot, fail) {
+//     console.log("Вы сделали выстрел...");
+
+//     setTimeout(function(){
+//         Math.random() > .5 ? headshot({}) : fail("Вы промахнулись");
+//     }, 3000)
+// };
+
+// function win() {
+//     console.log('Вы победили!');
+//     (drink == 1) ? buyBeer() : giveMoney();
+// }
+
+// function buyBeer() {
+//     console.log('Вам купили пива!');
+// }
+
+// function giveMoney() {
+//     console.log('Вам заплатили!');
+// }
+
+// function loose() {
+//     console.log('Вы програли!');
+// }
+
+// shoot({}, 
+//         function(mark) {
+//             console.log('Вы попали в цель!');
+//             win(mark, buyBeer, giveMoney);
+//         },
+//         function(miss){
+//             console.error(miss);
+//             loose();
+//         }
+//     )
